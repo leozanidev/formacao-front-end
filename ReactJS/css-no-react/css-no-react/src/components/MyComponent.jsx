61ee518cd6ed9fd0aa7976646474f7f971b8ffcd
Leonardo Zani de Souza <leonardozanidev@gmail.com>
2024-12-23 16:47:34 -0300

    Adicionada seção CSS NO REACT

---
+++ ReactJS/css-no-react/css-no-react/src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+// CSS no Componente
+import './MyComponent.css';
+
+function MyComponent() {
+  return (
+    <div>
+        <h1>CSS De Componente</h1>
+        <p>Esse parágrafo tem um estilo!</p>
+    </div>
+  );
+};
+
+export default MyComponent;
---
+++ ReactJS/css-no-react/css-no-react/src/components/Title.jsx
@@ -0,0 +1,10 @@
+// CSS Module
+import classes from "./Title.module.css"
+
+function Title() {
+  return (
+    <h1 className={classes.title}>Meu Título</h1>
+  );
+};
+
+export default Title;
